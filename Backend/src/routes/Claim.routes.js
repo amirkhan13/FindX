9bfe3eed7989572e31f80162c1e6b1829af278ea
amirkhan13 <amirkhan11691@gmail.com>
2025-10-29 09:47:42 +0530

    Remove JWT verification from routes and rely on role-based authorization for access control

--- Backend/src/routes/Claim.routes.js
+++ Backend/src/routes/Claim.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 import { createClaim, getAllClaims, updateClaimStatus, getReportedItems, getUserClaims } from "../controllers/claim.controller.js";
 import { authorizeRole } from "../middlewares/authorizeRole.middleware.js";
 
@@ -11,7 +11,7 @@ const router = Router()
 
 
 router.route("/:itemId/create-claim").post(
-    verifyJWT,
+
     authorizeRole('user', 'admin'),
     createClaim
 )
@@ -19,27 +19,27 @@ router.route("/:itemId/create-claim").post(
 
 
 router.route("/All-claims").get(
-    verifyJWT,
+
     authorizeRole('admin'),
     getAllClaims
 )
 
 router.route("/user-claims").get(
-    verifyJWT,
+
     authorizeRole('user', 'admin'),
     getUserClaims
 
 )
 
 router.route("/Item-Claims").get(
-    verifyJWT,
+
     authorizeRole('user', 'admin'),
     getReportedItems
 )
 
 
 router.route("/:claimId/status").patch(
-    verifyJWT,
+
     authorizeRole('user', 'admin'),
     updateClaimStatus
 )
